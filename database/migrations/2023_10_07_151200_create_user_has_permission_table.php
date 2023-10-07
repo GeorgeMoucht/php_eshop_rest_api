c74e0f1971b870ac05b1d87922cf56d20bb45b4f
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-07 18:22:05 +0300

    Create migration files for user groups and permissions

---
+++ database/migrations/2023_10_07_151200_create_user_has_permission_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_has_permission', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->timestamps();
+
+            // Define the primary key of the table.
+            $table->primary(['user_id'], 'group_id');
+
+            //Define foreign key constrains
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('groups')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_has_permission');
+    }
+};
