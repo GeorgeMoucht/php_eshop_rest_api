8d37b73f1db0dfeca59fd41cfb402e143e669c34
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-07 19:34:19 +0300

    Init order model and create table and relations

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
@@ -30,6 +31,11 @@ class Customer extends Model
     public function user(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 
 }
