56af16eb217351823ac95f0a4af32f811fc187fa
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-07 17:41:08 +0300

    Change the default attributes of User to what want

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'updated_at',
     ];
 
     /**
@@ -30,7 +30,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
