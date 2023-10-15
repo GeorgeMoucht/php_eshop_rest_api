bbf07e89d44a0d81746f1a71567a944917c64fd3
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-15 19:15:10 +0300

    Create necessary functions for to retrive permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'password'          => 'hashed',
     ];
 
     public function getJWTIdentifier(): mixed
@@ -56,20 +56,114 @@ class User extends Authenticatable implements JWTSubject
     }
 
     /**
-     * Establish one-to-many relationship with tables
+     * Establish one-to-many relationship with groups
      */
     public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class);
     }
 
+    /**
+     * Establish one-to-many relationship with permissions
+     */
     public function permissions(): BelongsToMany
     {
-        return $this->belongsToMany(Permission::class);
+        return $this->belongsToMany(Permission::class,);
     }
 
+    /**
+     * Establish one-to-one relationship with customers
+     */
     public function customer(): HasOne
     {
         return $this->hasOne(Customer::class);
     }
+
+    /**
+     *  Return the first role that his user has.
+     * @return bool
+     */
+    public function hasRoles(array $roles): bool
+    {
+
+        if ($this->groups->whereIn('name', $roles)->first()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get all permissions of this user form permission-user
+     * pivot table
+     * @return array
+     */
+    public function getAllPermissions(): array
+    {
+        return $this->permissions()->pluck('name')->toArray();
+    }
+
+    public function getAllRoles(): array
+    {
+        return $this->groups()->pluck('name')->toArray();
+    }
+
+    /**
+     *  Get all permssions from the assign role of the user.
+     * @return array
+     */
+    public function getAllPermissionsFromRoles(): array
+    {
+        return $this->groups->flatMap(function ($role) {
+            return $role->permissions->pluck('name');
+        })->unique()->toArray();
+    }
+
+    /**
+     * Get all permissions from group_permission and permissions_user
+     * tables.
+     * @return array
+     */
+    public function getAllPermissionsFromUserAndRoles(): array
+    {
+        return array_unique(array_merge($this->getAllPermissions(), $this->getAllPermissionsFromRoles()));
+    }
+
+    /**
+     * Check if a user has permission to make
+     * a specific call in the api.
+     *
+     * @param string $permission
+     * @return bool
+     */
+    public function hasPermission(string $permission, $permit = true): bool
+    {
+
+        $cacheName = 'user_id' . auth()->id() . '_has_permissions_' . $permission;
+
+        return cache()->rememberForever($cacheName, function () use ($permission) {
+            // check for individual permission assigned.
+            if ($this->permissions->where('name', $permission)->first()) {
+                return true;
+            }
+            // If any of the user roles that the user's roles has the permission
+            $rolePermissions = [];
+
+            //get all the roles / groups
+            foreach ($this->groups as $role) {
+                //get all the permissions for this role
+                foreach ($role->permissions as $perm) {
+                    //flush out all permissions
+                    $rolePermissions[] = $perm->name;
+                }
+            }
+
+            if ($role->permissions->whereIn('name', array_unique($rolePermissions))->first()) {
+                return true;
+            }
+
+            return false;
+        });
+    }
+
 }
