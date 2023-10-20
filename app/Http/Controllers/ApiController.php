db998cb2dff8e62567c3a2d40f668b4bea5e2e3d
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-20 15:00:05 +0300

    Init basic apicontroller class

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 // All the controllers will extend this class.
@@ -11,15 +12,18 @@ class ApiController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    protected function apiResponse(array $payload, int $status, $method, int $httpStatusCode = 200){
+    protected function apiResponse(array $payload, int $status, $method, int $httpStatusCode = 200): JsonResponse
+    {
         return response()->json($this->payload($payload, $status, $method), $httpStatusCode);
     }
 
-    protected function apiErrorResponse(array $payload, int $status, $method, int $httpStatusCode = 200){
+    protected function apiErrorResponse(array $payload, int $status, $method, int $httpStatusCode = 200): JsonResponse
+    {
         return response()->json($this->payload($payload, $status, $method, true), $httpStatusCode);
     }
 
-    private function payload(array $data, int $status, $method,  bool|int $error = false){
+    private function payload(array $data, int $status, $method,  bool|int $error = false): array
+    {
         return [
             'error' => $error,
             'data' => $data,  //main extention   []
