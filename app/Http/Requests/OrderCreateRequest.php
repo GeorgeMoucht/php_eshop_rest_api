f6c2c8a2e4f4f152b9f230a6d5975294f0db206b
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-23 15:28:41 +0300

    Make create request validator class for order.

---
+++ app/Http/Requests/OrderCreateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'comments' => 'sometimes|string|max:255',
+
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'text' => 'The type of the field must be string.',
+        ];
+    }
+}
