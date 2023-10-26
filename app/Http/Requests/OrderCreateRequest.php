d19537b5017167f5e33e5a09c3005926940bd7d3
GeorgeMoucht <giorgosmoucht@gmail.com>
2023-10-26 18:33:45 +0300

    work with order

--- app/Http/Requests/OrderCreateRequest.php
+++ app/Http/Requests/OrderCreateRequest.php
@@ -22,15 +22,23 @@ class OrderCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'comments' => 'sometimes|string|max:255',
-
+            'comments'                  => 'sometimes|string|max:255',
+            'order'                     => 'required|array',
+            'order.*.product_id'        => 'required|integer',
+            'order.*.quantity_ordered'  => 'required|integer',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'text' => 'The type of the field must be string.',
+            'comments.string'                       => 'Comments must be type of string.',
+            'order.required'                        => 'Thew order field is required.',
+            'order.array'                           => 'The order must be type of array.',
+            'order.*.product_id.required'           => 'Each order product item must have a product_id.',
+            'order.*.product_id.integer'            => 'The product_id must be type of integer.',
+            'order.*.quantity_ordered.required '    => 'Each product should have quantity_ordered attribute.',
+            'order.*.quantity_ordered.integer'      => 'The quantity_ordered must be type of integer.'
         ];
     }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -15,16 +15,25 @@ class OrderService
 
     public function create(array $data)
     {
+        dd($data);
         if(!me()->customer) {
             return null;
         }
+
+
+
         $order = new Order();
         $order->customer_id = getCustomerId();
         $order->order_date = now();
         $order->status = Status::PENDING->value;
         $order->fill($data);
+
+
+        // TODO For each product in $data array. Fill order_details table.
+
         if($order->save()) {
-            return $order->save();
+
+//            return $order->save();
         }
         return false;
     }
